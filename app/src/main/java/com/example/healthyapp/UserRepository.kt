f10a004a4ba58d339a7dfe49e84bc12f2f9fbf44
Anastasiya <alitvinova@baccasoft.ru>
2019-11-06 12:55:40 +0400

    userRepository

---
+++ app/src/main/java/com/example/healthyapp/UserRepository.kt
@@ -0,0 +1,7 @@
+package com.example.healthyapp
+
+import com.example.healthyapp.db.model.entity.User
+
+interface UserRepository {
+    fun getUserByLogin(login: String): User?
+}
---
+++ app/src/main/java/com/example/healthyapp/UserRepositoryImpl.kt
@@ -0,0 +1,11 @@
+package com.example.healthyapp
+
+import com.example.healthyapp.db.model.entity.User
+
+open class UserRepositoryImpl : UserRepository {
+
+
+    override fun getUserByLogin(login: String): User? {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
