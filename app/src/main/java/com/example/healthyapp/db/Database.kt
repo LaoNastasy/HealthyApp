1cf13c350ed61f6b7f844b1e586b48d6f5124ee9
Anastasiya <nastasiya97@mail.ru>
2020-02-15 15:09:52 +0400

    удалены ненужные сущности

--- app/src/main/java/com/example/healthyapp/db/Database.kt
+++ app/src/main/java/com/example/healthyapp/db/Database.kt
@@ -8,21 +8,15 @@ import com.example.healthyapp.db.model.entity.*
 @Database(
     entities = [
         Placement::class,
-        PlacementCondition::class,
-        ReferenceIndicators::class,
         User::class,
-        UserCondition::class,
         WorkplaceUser::class,
         Workplace::class
     ], version = 1
 )
 abstract class Database : RoomDatabase() {
 
-    abstract fun getPlacementConditionDao(): PlacementConditionDao
     abstract fun getPlacementDao(): PlacementDao
-    abstract fun getReferenceIndicatorsDao(): ReferenceIndicatorsDao
     abstract fun getUserDao(): UserDao
-    abstract fun getUserConditionDao(): UserConditionDao
     abstract fun getWorkplaceUserDao(): WorkplaceUserDao
     abstract fun getWorkplaceDao():WorkplaceDao
 }
