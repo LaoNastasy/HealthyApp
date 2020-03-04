4a62193272caa5f637c50d6e28633b1df3934fcc
Anastasiya <alitvinova@baccasoft.ru>
2020-03-04 12:42:22 +0400

    подключила Stetho для мониторинга бд

--- app/src/main/java/com/example/healthyapp/App.kt
+++ app/src/main/java/com/example/healthyapp/App.kt
@@ -2,13 +2,14 @@ package com.example.healthyapp
 
 import android.app.Application
 import com.example.healthyapp.di.DI
+import com.facebook.stetho.Stetho
 
 class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
         DI.init(applicationContext)
-
+        Stetho.initializeWithDefaults(this)
     }
 
 }
